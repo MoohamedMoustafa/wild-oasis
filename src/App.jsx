2e163f0d481e3060e3cd2e86128af1f62cec7cc6
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2026-01-29 18:47:57 +0200

    implement styled-components for UI elements and add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const Title = styled.h1`
+  font-size: 1.5rem;
+  color: blue;
+  text-align: center;
+  font-weight: 600;
+  background-color: #f0f0f0;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 1.25rem;
+`;
+
 export default function App() {
-  return <div>Hello My APP</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Title>Hello, World!</Title>
+        <Button onClick={() => console.log("Button clicked!")}>Check in</Button>
+        <Button onClick={() => console.log("Button clicked!")}>
+          Check out
+        </Button>
+        <Input placeholder="guest number" type="number" />
+        <Input placeholder="guest number" type="number" />
+      </StyledApp>
+    </>
+  );
 }
