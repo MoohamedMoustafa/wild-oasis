7e62d12fa299257d5214d6fca81dd4b14b14ce93
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2026-01-31 12:34:41 +0200

    feat: Set up core application structure with React Router, add Cabins page with a table, and integrate React Query.

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,23 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+        // this is cashTime in older version
+        gcTime: 60 * 1000,
+      },
+    },
+  });
+
   return (
-    <>
+    <QueryClientProvider client={client}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,6 +42,6 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
